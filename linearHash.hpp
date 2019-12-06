7c0d67f7a6295f63e7b64c0806ff028287bf37e6
kaed1046 <58530877+kaed1046@users.noreply.github.com>
2019-12-05 22:30:39 -0700

    Create linearHash.hpp

---
+++ linearHash.hpp
@@ -0,0 +1,29 @@
+#ifndef HASH_HPP
+#define HASH_HPP
+#include <string>
+using namespace std;
+
+
+class HashTable
+{
+    const int static tableSize = 10009;  // No. of buckets (linked lists)
+
+    // Pointer to an array containing buckets
+    int table[tableSize];
+
+    int numAdded;
+
+public:
+    int loadFactor;
+    HashTable();  // Constructor
+    // inserts a key into hash table
+    bool insertItem(int key);
+    bool deleteItem(int key);
+    // hash function to map values to key
+    unsigned int hashFunction(int key);
+    void printTable();
+    int Lookup(int key);
+    void calcLoadFactor();
+};
+
+#endif
