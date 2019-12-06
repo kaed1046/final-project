db65cf3b151c086bf27525ee0d98252a74847f39
kaed1046 <58530877+kaed1046@users.noreply.github.com>
2019-12-05 22:33:27 -0700

    Create linear1.cpp

---
+++ linear1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "linearHash.hpp"
+#include <sstream>
+#include <fstream>
+#include <istream>
+using namespace std;
+
+
+
+HashTable::HashTable()
+{
+    for(int i=0; i < tableSize; i++)
+        table[i] = -1;
+    numAdded = 0;
+    loadFactor = 0;
+}
+
+unsigned int HashTable::hashFunction(int key)
+{
+  int index;
+  index = key % tableSize;
+  return index;
+}
+
+bool HashTable::insertItem(int key)
+{
+  int place;
+  place = hashFunction(key); //produces the ideal index
+  for(int i = place; i < tableSize; i++) //starts at ideal index and moves up by one
+  {
+    if(table[i] == -1) //if it's an empty spot
+    {
+      table[i] = key; //place the info in the array
+      numAdded++;
+      calcLoadFactor();
+      return true;
+    }
+  }
+  for(int i = 0; i < place; i++)
+  //if from the ideal spot to the end is full, start at zero and check for
+  //empty spots
+  {
+    if(table[i] == -1)
+    {
+      table[i] = key;
+      numAdded++;
+      calcLoadFactor();
+      return true;
+    }
+  }
+  //if there are no empty spots
+  return false;
+}
+
+int HashTable::Lookup(int key)
+{
+  int index = hashFunction(key);
+  for(int i = index; i < tableSize; i++)
+  {
+    if(table[i] == key)
+    {
+      return i;
+    }
+  }
+  for(int i = 0; i < index; i++)
+  {
+    if(table[i] == key)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+
+bool HashTable::deleteItem(int key)
+{
+  int delIndex;
+  delIndex = Lookup(key);
+  if (delIndex == -1)
+  {
+    cout << "Key not found" << endl;
+    return false;
+  }
+  else
+  {
+    table[delIndex] = -1;
+    return true;
+  }
+}
+
+void HashTable::calcLoadFactor()
+{
+  loadFactor = numAdded / tableSize;
+  return;
+}
